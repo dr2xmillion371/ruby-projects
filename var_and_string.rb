24e2f1c8ab8f7b7abffa1db95f0efd09c96ae885
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-04-09 10:47:42 -0500

    var and string

---
+++ var_and_string.rb
@@ -0,0 +1,22 @@
+# start
+# first set of variables
+
+
+x, y, z = 1, 2, 3
+
+puts "The first variable is equal to 1"
+puts "The second variable is equal to 2"
+puts "The third variable is equal to 3"
+
+
+
+# the second set of variables
+
+a, b, c = 4, 5, 6
+
+puts "the first variable in the second set is 4"
+puts "the second variable in the second set is 5"
+puts "the third variable in the second set is 6"
+
+
+# end
